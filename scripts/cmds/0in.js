2b8dfdaa9802f96d65b64ac4b3db839a654acd5b
rbkhanok <rbkhan794@gmail.com>
2024-07-01 19:17:09 +0600

    Update 0in.js

--- scripts/cmds/0in.js
+++ scripts/cmds/0in.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "in",
     aliases: ["inboxme", "in"],
     version: "1.0",
-    author: "RB-BADOL-KHAN",
+    author: "Unknown",
     countDown: 10,
     role: 0,
     shortDescription: {
@@ -27,9 +27,9 @@ module.exports = {
   },
   onStart: async function({ api, event, args, message }) {
     try {
-      const query = encodeURIComponent(args.join(' '));
-      message.reply("☑ |✦ 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹 𝗬𝗼𝘂𝗿 𝗜𝗻𝗯𝗼𝘅 𝗧𝗲𝘅𝘁\n𝗖𝗵𝗲𝗰𝗸 𝗜𝗻𝗯𝗼𝘅", event.threadID);
-      api.sendMessage("👀 |✦ 𝗛𝗲𝗹𝗹𝗼 𝗝𝗮𝗻", event.senderID);
+       const attachment = await global.utils.getStreamFromURL("https://i.imgur.com/iWDVSOY.jpeg");
+        await message.reply({ body: "☑ |✦ 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹 𝗬𝗼𝘂𝗿 𝗜𝗻𝗯𝗼𝘅 𝗧𝗲𝘅𝘁\n𝗖𝗵𝗲𝗰𝗸 𝗜𝗻𝗯𝗼𝘅"} attachment });
+        await api.sendMessage("👀 |✦ 𝗛𝗲𝗹𝗹𝗼 𝗝𝗮𝗻", event.senderID);
     } catch (error) {
       console.error("Error bro: " + error);
     }
