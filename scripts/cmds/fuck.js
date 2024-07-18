5c7b02f945891b3e110abbd141a732e17ff7f645
rbkhanok <rbkhan794@gmail.com>
2024-07-18 18:43:31 +0600

    Update fuck.js

--- scripts/cmds/fuck.js
+++ scripts/cmds/fuck.js
@@ -41,7 +41,7 @@ async function bal(one, two) {
     let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
     avtwo.circle()
     let pth = "spiderman.png"
-    let img = await jimp.read("https://i.imgur.com/vpUme67.jpeg")
+    let img = await jimp.read("https://drive.google.com/uc?id=1KmOACtYuNozeVOsTfth9-x19WSHfYthT")
     img.resize(1440, 1080).composite(avone.resize(200, 200), 380, 50).composite(avtwo.resize(200, 200), 800, 100);
 
     await img.writeAsync(pth)
