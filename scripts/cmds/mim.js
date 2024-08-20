95982472546c2599fdac8f53b23225924c29381c
rbkhanok <rbkhan794@gmail.com>
2024-08-21 00:12:18 +0600

    Update mim.js

--- scripts/cmds/mim.js
+++ scripts/cmds/mim.js
@@ -1,40 +1,120 @@
-const fs = require("fs-extra");
 const axios = require("axios");
-const moment = require("moment-timezone");
 
-module.exports = {
-  config: {
-    name: "mim",
-    version: "1.0",
-    author: "★𝐌𝟙𝐇𝟜𝐌𝐌𝟜𝐃-𝐁𝟒𝐃𝟗𝐋★",
-    countDown: 5,
-    role: 0,
-    Description: "no-prefix",
-    category: "no prefix",
-    guide: {
-      en: "{p}{n}",
-    },
-  },
+const baseApiUrl = async () => {
+  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
+  return base.data.api;
+};
 
-  onStart: async function({}) {},
+module.exports.config = {
+  name: "mim",
+  version: "1.0.0",
+  role: 0,
+  author: "dipto & Badol", //modified by Badol
+  description: "better than all Sim simi with multiple conversation",
+  guide: { en: "[message]" },
+  category: "ChatBots",
+  coolDowns: 5,
+};
 
-  onChat: async function({ api, event, args, Threads, usersData }) {
-    const data = await usersData.get(event.senderID);
-    const name = data.name;
-    const { threadID, messageID, senderID, body } = event;
-    const time = moment.tz("Asia/Dhaka").format("hh:mm:ss L");
+module.exports.onReply = async function ({ api, event }) {
+  if (event.type === "message_reply") {
+    const reply = event.body.toLowerCase();
+    if (isNaN(reply)) {
+      try {
+        const baseUrl = await baseApiUrl();
+        const response = await axios.get(`${baseUrl}/baby?text=${encodeURIComponent(reply)}`);
+        const ok = response.data.reply;
+        await api.sendMessage(
+          ok,
+          event.threadID,
+          (error, info) => {
+            if (!error) {
+              global.GoatBot.onReply.set(info.messageID, {
+                commandName: this.config.name,
+                type: "reply",
+                messageID: info.messageID,
+                author: event.senderID,
+                link: ok,
+              });
+            }
+          },
+          event.messageID,
+        );
+      } catch (error) {
+        console.error(`Failed to get a reply: ${error.message}`);
+        api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+      }
+    }
+  }
+};
 
-    if (event.body.indexOf("mim")==0 || (event.body.indexOf("Mim")==0) ||
-event.body.indexOf("মিম")==0) {
-      const messages = [
-        "বেশি mim mim করলে চুম্মা দিয়া তোমার কিডনি ব্লক করে দেবো😒 " , "শুনবো না😼তুমি আমাকে প্রেম করাই দাও নাই🥺পচা তুমি🥺" , "আমি আবাল দের সাথে কথা বলি না,ok😒" , "এতো ডেকো না,প্রেম এ পরে যাবো তো🙈" , "Bolo Babu, তুমি কি আমাকে ভালোবাসো? 🙈💋 " , "বার বার ডাকলে মাথা গরম হয়ে যায় কিন্তু😑", "হ্যা বলো😒, তোমার জন্য কি করতে পারি😐😑?" , "এতো ডাকছিস কেন?গালি শুনবি নাকি? 🤬" , "I love you janu🥰" , "আরে Bolo আমার জান ,কেমন আছো?😚 " , "এত যে মিম করো তোমরা কি আমার ভাতার লাগে 🥴" , "Hop beda😾,Boss বল boss😼" , "চুপ থাক ,নাই তো তোর দাত ভেগে দিবো কিন্তু" , ", মিম না ভাবি বল ভাবি 😘 " , "বার বার Disturb করছিস কোনো😾,আমার জানুর সাথে ব্যাস্ত আছি😋" , "বোকাচোদা এতো ডাকিস কেন🤬" , "আমাকে ডাকলে ,আমি কিন্তু কিস করে দিবো😘 " , "আমারে এতো ডাকিস না আমি মজা করার mood এ নাই এখন😒" , "হ্যাঁ জানু , এইদিক এ আসো কিস দেই🤭 😘" , "দূরে যা, তোর কোনো কাজ নাই, শুধু মিম মিম করিস  😉😋🤣" , "তোর কথা তোর বাড়ি কেউ শুনে না ,তো আমি কোনো শুনবো ?🤔😂 " , "আমাকে ডেকো না,আমি ব্যাস্ত আছি" , "কি হলো , মিস্টেক করচ্ছিস নাকি🤣" , "বলো কি বলবা, সবার সামনে বলবা নাকি?🤭🤏" , "কালকে দেখা করিস তো একটু 😈" , "হা বলো, শুনছি আমি 😏" , "আর কত বার ডাকবি ,শুনছি তো" , "হুম বলো কি বলবে😒" , "বলো কি করতে পারি তোমার জন্য" , "আমি তো অন্ধ কিছু দেখি না🐸 😎" , "আর একবার আমার নাম ধরে ডাকলে তোরে কোলে করে বেডরুমে নিয়ে আসব 😉" , "বলো জানু 🌚" , "তোর কি চোখে পড়ে না আমি ব্যাস্ত আছি😒","হুম জান তোমার ওই খানে উম্মহ😑😘" , "আহ শুনা আমার তোমার অলিতে গলিতে উম্মাহ😇😘" , " jang hanga korba😒😬" , "হুম জান তোমার অইখানে উম্মমাহ😷😘" , "আসসালামু আলাইকুম বলেন আপনার জন্য কি করতে পারি..!🥰" , "আমাকে এতো না ডেকে বস বাদল চৌধুরী কে একটা গফ দে 🙄" , "আমাকে এতো না ডেকে বস রাজা বাবু কে একটা গফ দে 🙄"
-      ];
-      const rand = messages[Math.floor(Math.random() * messages.length)];
-      
-      const msg = {
-        body: `【•${name}•】\n\n${rand}`,attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=1X-rlSqgtVi-cI1hyoOyA2W4_mUpec7zv")
-      };
-      api.sendMessage(msg, threadID, messageID);
-}
-}
+module.exports.onChat = async function ({ event, api }) {
+  const body = event.body && event.body.toLowerCase().trim();
+  if (body === "mim") {
+    api.sendMessage("🫰উত্তর দিতে একটি প্রশ্ন প্রদান করুন🫰\n\nExample:\n\nmim ki koro", event.threadID, event.messageID);
+  } else if (body.startsWith("mim ")) {
+    const query = body.slice(4).trim();
+    if (query) {
+      try {
+        const baseUrl = await baseApiUrl();
+        const response = await axios.get(`${baseUrl}/baby?text=${encodeURIComponent(query)}`);
+        const reply = response.data.reply;
+        await api.sendMessage(
+          { body: reply },
+          event.threadID,
+          (error, info) => {
+            if (!error) {
+              global.GoatBot.onReply.set(info.messageID, {
+                commandName: this.config.name,
+                type: "reply",
+                messageID: info.messageID,
+                author: event.senderID,
+                link: reply,
+              });
+            }
+          },
+          event.messageID,
+        );
+      } catch (error) {
+        console.error(`Failed to get an answer: ${error.message}`);
+        api.sendMessage(`Error: ${error.message}.\nAn error occurred.`, event.threadID, event.messageID);
+      }
+    }
+  }
+};
+
+module.exports.onStart = async function ({ api, args, event }) {
+  try {
+    const query = args.join(" ").toLowerCase();
+    if (!query) {
+      api.sendMessage(
+        "Please provide a question to answer\n\nExample:\nbaby ki koro",
+        event.threadID,
+        event.messageID,
+      );
+      return;
+    }
+    const baseUrl = await baseApiUrl();
+    const response = await axios.get(`${baseUrl}/baby?text=${encodeURIComponent(query)}`);
+    const reply = response.data.reply;
+    await api.sendMessage(
+      { body: reply },
+      event.threadID,
+      (error, info) => {
+        if (!error) {
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            type: "reply",
+            messageID: info.messageID,
+            author: event.senderID,
+            link: reply,
+          });
+        }
+      },
+      event.messageID,
+    );
+  } catch (error) {
+    console.error(`Failed to get an answer: ${error.message}`);
+    api.sendMessage(`${error.message}.\nAn error occurred.`, event.threadID, event.messageID);
+  }
 };
